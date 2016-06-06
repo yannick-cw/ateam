19b8ce975abf9ba5d1c244809fb01a981d411a3e
437580 <yannick.gladow@holidaycheck.com>
2016-06-06 15:23:30 +0200

    refactor

--- src/main/scala/elasticserach_API/Requests.scala
+++ src/main/scala/elasticserach_API/Requests.scala
@@ -15,22 +15,6 @@ import scala.util.Failure
 trait Requests extends HttpRequester with Protocols {
   val (rep, dem) = ("rep", "dem")
 
-  def insert(cleanedDoc: CleanedDoc): Future[HttpResponse] = {
-    val index = matchIndex(cleanedDoc)
-    val docType = cleanedDoc.src.toLowerCase
-
-    index match {
-      case Some(str) =>
-        val request = RequestBuilding.Post(s"/$str/$docType/",
-          entity = HttpEntity(ContentTypes.`application/json`, cleanedDoc.toJson.compactPrint))
-        futureHttpResponse(request, "172.17.0.2", 9200)
-
-      case None =>
-        import scala.concurrent.ExecutionContext.Implicits.global
-        Future(throw new IllegalArgumentException("Undefined source"))
-    }
-  }
-
   def bulkInsert(docs: Seq[CleanedDoc]): Future[HttpResponse] = {
     val querie = docs.map{ doc =>
       //todo
@@ -38,18 +22,24 @@ trait Requests extends HttpRequester with Protocols {
       val docType = doc.src.toLowerCase
       s"""{ "index": { "_index": "$index", "_type": "$docType" }}\n${doc.toJson.compactPrint}\n"""
     }.mkString
-//    println(querie)
 
     val request = RequestBuilding.Post(s"/_bulk",
       entity = HttpEntity(ContentTypes.`application/json`, querie))
-    futureHttpResponse(request, "172.17.0.2", 9200)
+    futureHttpResponse(request, "localhost", 9200)
   }
 
-  private def matchIndex(cleanedDoc: CleanedDoc): Option[String] = cleanedDoc.src match {
-    case "Republicans" => Some(rep)
-    case "Republican" => Some(rep)
-    case "democrats" => Some(dem)
-    case "Democrat" => Some(dem)
-    case any => None
+  private def matchIndex(cleanedDoc: CleanedDoc): Option[String] = {
+
+    val subRedditToIndex = Map(
+      "republicans" -> rep,
+      "Republican" -> rep,
+      "Conservative" -> rep,
+      "democrats" -> dem,
+      "Liberal" -> dem,
+      "Democrat" -> dem,
+      "SandersForPresident" -> dem
+    )
+
+    subRedditToIndex.get(cleanedDoc.src)
   }
 }
